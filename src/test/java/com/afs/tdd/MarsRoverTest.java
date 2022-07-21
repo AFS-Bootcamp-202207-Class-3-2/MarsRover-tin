e61def9921d75c6bc7ca85c70bf997bfbd8b5867
Tin <chenti11@oocl.com>
2022-07-21 16:47:04 +0800

    test: execute L when rover in (0, 0, N)

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -20,5 +20,20 @@ class MarsRoverTest {
 
         assertEquals("N", rover.getDirection());
     }
+
+    @Test
+    void should_go_to_0_0_W_when_getMarsRoverReport_given_0_0_N() {
+        //given
+        MarsRover rover = new MarsRover(0, 0, "N");
+
+        //when
+        rover.getMarsRoverReport("L");
+        //then
+        assertEquals(0, rover.getxPosition());
+
+        assertEquals(0, rover.getyPosition());
+
+        assertEquals("W", rover.getDirection());
+    }
     
 }
